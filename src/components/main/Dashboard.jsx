700e59f13e3b9d79312a2836ff7e4e39c9cf3d8f
rohitchoudhary99 <rohit.choudhary@softradix.in>
2025-05-15 15:28:28 +0530

    redux added

--- src/components/main/Dashboard.jsx
+++ src/components/main/Dashboard.jsx
@@ -1,6 +1,11 @@
 import Table from "../../shared/pages/table/table";
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from '../../redux/counterSlice';
 
 const Dashboard = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   const data = [
     { id: 1, name: "John", age: 25 },
     { id: 2, name: "Jane", age: 30 },
@@ -9,6 +14,9 @@ const Dashboard = () => {
   const columns = ["id", "name", "age"];
   return (
     <div>
+        <h2>Count: {count}</h2>
+      <button onClick={() => dispatch(increment())}> + </button>
+      <button onClick={() => dispatch(decrement())}> - </button>
       <h1>---</h1>
       <Table data={data} columns={columns} />
     </div>
